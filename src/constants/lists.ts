93a833a1f0def482eb48fd45ac44b9f08d330ed1
shuchen0418 <2537400754@qq.com>
2024-12-15 21:09:14 +0800

    pledge-front-init

---
+++ src/constants/lists.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_TOKEN_LIST_URL = 'https://dev-v2-backend.pledger.finance/api/v21/token?chainId=97'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  "https://dev-v2-backend.pledger.finance/api/v21/token?chainId=97",
+"https://dev-v2-backend.pledger.finance/api/v21/token?chainId=56"
+]
